f7197163cfacda69fedc30ad4d576dec33b0319f
Peca <peca@mycity.rs>
2026-03-30 15:22:28 +0200

    Add functionality to install-post.sh for handling override files

--- hooks/install-post.sh
+++ hooks/install-post.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-# Nothing to do here for now
+if [ -d "override" ]; then
+    cp override/* /usr/local/bin/
+    echo "= Copied override files to /usr/local/bin/"
+fi
+
+if [ -f "/usr/local/bin/devops-override-conf" ]; then
+    chown root:root /usr/local/bin/devops-override-conf
+    chmod 0644 /usr/local/bin/devops-override-conf
+    echo "= Set permissions for /usr/local/bin/devops-override-conf"
+fi
+
